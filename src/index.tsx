6d44a3a7da4255fa01facdb6f49df62e5c9414db
wuguang <wuguang@wxblockchain.com>
2021-09-26 11:12:40 +0800

    init program

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import React from './myReact/react';
+
+
+console.log(start());
+
+
+function start(){
+    return <div>
+        <h1>hello world~~</h1> 
+        <h2>hello two ~~~</h2>
+    </div>
+}
---
+++ src/myReact/react.ts
@@ -0,0 +1,14 @@
+
+const createElement = (type,config,...children)=>{
+        
+    return {
+
+    }
+}
+
+const React = {
+    createElement,
+};
+
+
+export default React;
