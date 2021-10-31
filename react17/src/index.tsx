a73fc4052455ba889496c9f8bbba7cb1f8588924
2012wuguang <wuguang_123@163.com>
2021-10-31 18:09:52 +0800

    update

--- react17/src/index.tsx
+++ react17/src/index.tsx
@@ -7,10 +7,20 @@ const {
 } = React as any;
 
 import MainPage from './mainPage';
+import SetTimer from './components/setTimer';
+import SetTimer02 from './components/setTimer02';
+import SetTimer07 from './components/setTimer07';
 const root = document.getElementById("react-fiber-root");
 
-//ReactDom.render(<MainPage />,root);
+ReactDom.render(<>
+    <SetTimer />
+    <SetTimer02 />
+    <SetTimer07 />
+</>,root);
 
+/*
 createRoot(root).render(<ConcurrentMode>
     <MainPage />
+    <SetTimer />
 </ConcurrentMode>);
+*/
