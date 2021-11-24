4ac66ae1dde6334558b8f947217a9e6b7169a4f6
wuguang <wuguang@wxblockchain.com>
2021-11-24 09:46:45 +0800

    update

--- myHooks/src/hooks.ts
+++ myHooks/src/hooks.ts
@@ -5,6 +5,7 @@ interface Fiber {
     child?:Fiber;
     sibling?:Fiber;
     return?:Fiber;
+    alternate:Fiber;
     memoizedState?:any;
     updateQueue:any;
 }
@@ -12,16 +13,26 @@ interface Fiber {
 interface Hook{
     memoizedState: any;
     baseState: any;
-    baseQueue: UpdateQueue<any,any>|null;
+    baseQueue: Update<any, any> | null;
     queue: any;
     next: Hook|null;
 }
 
 
+/*
+源码结构
+type Update<S, A> = {|
+  lane: Lane,
+  action: A,
+  hasEagerState: boolean,
+  eagerState: S | null,
+  next: Update<S, A>,
+|};
+*/
 
 type Update<State,A> = {
-    tag?: 0 | 1 | 2 | 3,
-    payload?: any,
+    //tag?: 0 | 1 | 2 | 3,
+    //payload?: any,
     callback?: Function,
     hasEagerState?: boolean,
     eagerState?: State | null,
@@ -48,6 +59,7 @@ type UpdateQueue<S,A> = {
     lastRenderedState:S|null;
     last:Update<S,A>;
     dispatch:(A)=>S|null;
+
 };
   
 type SharedQueue<State>= {
@@ -56,30 +68,35 @@ type SharedQueue<State>= {
     lanes: any;
 }
 
-
 let currentlyRenderingFiber:Fiber = null;
 let ReactCurrentDispatcher = {
     current:null,
 }
 let workInProgressHook = null;
+let currentHook:Hook = null;
 
 
 //初始化时用这个
 const HooksDispatcherOnMount = { 
     useState: mountState,
-    useEffect: mountEffect,
     useReducer:mountReducer,
+    useEffect: mountEffect
 }
 
 const HooksDispatcherOnUpdate = {
     useState: updateState,
-    useEffect: updateEffect,
     useReducer:updateReducer,
+    useEffect: updateEffect
 }
 
 function is(objA,objB){
     return JSON.stringify(objA) === JSON.stringify(objB);
 }
+
+/*
+作用，
+将一个action(payload),构建成一个update对象，将update对象挂载到queue对象上，形成环状链表；
+*/
 
 function dispatchAction<A,State>(fiber:Fiber, queue:UpdateQueue<State,A>, action){
     /* 第一步：创建一个 update */
@@ -99,15 +116,15 @@ function dispatchAction<A,State>(fiber:Fiber, queue:UpdateQueue<State,A>, action
     }
     queue.pending = update;
 
-
     const lastRenderedReducer = queue.lastRenderedReducer;
     const currentState = queue.lastRenderedState;                 /* 上一次的state */
     const eagerState = lastRenderedReducer(currentState, action); /* 这一次新的state */
-    if (is(eagerState,currentState)) {                           /* 如果每一个都改变相同的state，那么组件不更新 */
+    if (is(eagerState,currentState)) {                            /* 如果每一个都改变相同的state，那么组件不更新 */
        return 
     }
-    scheduleUpdateOnFiber(fiber);   
 
+    //开始下一轮调度
+    scheduleUpdateOnFiber(fiber);   
 }
 
 //useState 第一次，返回[value,setValue]
@@ -120,17 +137,16 @@ function mountState(initialState){
     } 
     //初始化赋值
     hook.memoizedState = hook.baseState = initialState;
-
     //初始化queue 为空 {}
     const queue: UpdateQueue<any,any> = {
         pending: null,
         dispatch: null,
         last:null,
+        //返回明确的action,即payload
         lastRenderedReducer: basicStateReducer,
-        //上一次的 state 
+        //记住上一次的 state，这里的state属于queue,queue属于明确的hook，hook属于某个明确的fiber，fiber绑定某个明确的节点 
         lastRenderedState: initialState
     };
-
     //dispatch 更新调度当前的queue
     const dispatch = dispatchAction.bind(null,currentlyRenderingFiber,queue); 
     queue.dispatch = dispatch;
@@ -139,49 +155,130 @@ function mountState(initialState){
 
 
 //源码如此
+//返回action 本身或称之为payload
 function basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
     // $FlowFixMe: Flow doesn't like mixed types
     return typeof action === 'function' ? action(state) : action;
 }
-  
 
 function mountEffect(){
 
 }
 
 function mountReducer(){
+    // There
 
 }
 
+//于BasicStateAction类型一致
+type initStateType <S> = () => S | S;
 
-
-
-
-  
 //源码如此
-function updateState(initValue){
-    return updateReducer(basicStateReducer, (initialState: any));
+//不做类型限制了
+
+function updateState(){
+    return updateReducer(basicStateReducer);
 }
 
-function updateReducer(){
-    // 第一步把待更新的pending队列取出来。合并到 baseQueue
-    const first = baseQueue.next;
-    let update = first;
-    do {
-        /* 得到新的 state */
-        newState = reducer(newState, action);
-    } while (update !== null && update !== first);
-    hook.memoizedState = newState;
+//重新取值而已，合并，清空队列
+//忽略了优先级 等
+function updateReducer(reducer){
+    const hook = updateWorkInProgressHook();
+    const {queue} = hook;
+
+    //赋值reducer？？？why,不是有了么？
+    queue.lastRenderedReducer = reducer;
+
+    const current: Hook = currentHook;
+    const pendingQueue = queue.pending;
+
+    if(pendingQueue!==null){
+        let first = pendingQueue.next;//第一个更新对象
+        let newState = current.memoizedState;//拿到老状态
+
+        let update = first;
+        //将 pendingQueue 里的任务消耗调，得到最新的状态
+        do{
+            const action = update.action;//action：就是传的参数，例如setState('参数')
+            newState = reducer(newState,action);//计算新状态，因为如果传的是函数，要依赖老状态
+            update = update.next;
+        }while(update !== null && update !== first);
+        //清空整理 指针
+        queue.pending = null;//更新过了可以清空更新环形链表
+        hook.memoizedState =  newState;//让新的hook对象的memoizedState等于计算的新状态    
+        queue.lastRenderedState = newState;//把新状态也赋值给lastRenderedState一份
+    }
+    const dispatch = dispatchAction.bind(null, currentlyRenderingFiber, queue);
     return [hook.memoizedState, dispatch];
+
+    //第一步把待更新的pending队列取出来
 }
 
+function updateWorkInProgressHook(): Hook {
+    // This function is used both for updates and for re-renders triggered by a
+    // render phase update. It assumes there is either a current hook we can
+    // clone, or a work-in-progress hook from a previous render pass that we can
+    // use as a base. When we reach the end of the base list, we must switch to
+    // the dispatcher used for mounts.
+    let nextCurrentHook: null | Hook;
+    if (currentHook === null) {
+      const current = currentlyRenderingFiber.alternate;
+      if (current !== null) {
+        nextCurrentHook = current.memoizedState;
+      } else {
+        nextCurrentHook = null;
+      }
+    } else {
+      nextCurrentHook = currentHook.next;
+    }
+  
+    let nextWorkInProgressHook: null | Hook;
+    if (workInProgressHook === null) {
+      nextWorkInProgressHook = currentlyRenderingFiber.memoizedState;
+    } else {
+      nextWorkInProgressHook = workInProgressHook.next;
+    }
+  
+    if (nextWorkInProgressHook !== null) {
+      // There's already a work-in-progress. Reuse it.
+      workInProgressHook = nextWorkInProgressHook;
+      nextWorkInProgressHook = workInProgressHook.next;
+  
+      currentHook = nextCurrentHook;
+    } else {
+      // Clone from the current hook.
+  
+      if (nextCurrentHook === null) {
+        throw new Error('Rendered more hooks than during the previous render.');
+      }
+  
+      currentHook = nextCurrentHook;
+  
+      const newHook: Hook = {
+        memoizedState: currentHook.memoizedState,
+  
+        baseState: currentHook.baseState,
+        baseQueue: currentHook.baseQueue,
+        queue: currentHook.queue,
+  
+        next: null,
+      };
+  
+      if (workInProgressHook === null) {
+        // This is the first hook in the list.
+        currentlyRenderingFiber.memoizedState = workInProgressHook = newHook;
+      } else {
+        // Append to the end of the list.
+        workInProgressHook = workInProgressHook.next = newHook;
+      }
+    }
+    return workInProgressHook;
+}
+  
 
 function updateEffect(){
 
 }
-
-
-
 
 function mountWorkInProgressHook() {
     const hook:Hook = { memoizedState: null, baseState: null, baseQueue: null,queue: null, next: null};
@@ -195,14 +292,6 @@ function mountWorkInProgressHook() {
 
 
 
-/*
-const  HooksDispatcherOnUpdate ={
-   useState:updateState,
-   useEffect: updateEffect,
-   ...
-}
-*/
-
 function renderWithHooks(current,workInProgress,Component,props){
     currentlyRenderingFiber = workInProgress;
     workInProgress.memoizedState = null; /* 每一次执行函数组件之前，先清空状态 （用于存放hooks列表）*/
@@ -213,6 +302,8 @@ function renderWithHooks(current,workInProgress,Component,props){
     //清空当前作用域 hook
     workInProgressHook = null;
     let children = Component(props);   /* 执行我们真正函数组件，所有的hooks将依次执行。 */
+    
+    //这里不设置
     //ReactCurrentDispatcher.current = ContextOnlyDispatcher;  /* 将hooks变成第一种，防止hooks在函数组件外部调用，调用直接报错。 */
 }
 
@@ -220,3 +311,10 @@ function  scheduleUpdateOnFiber(fiber){
     //开始调度
     //......
 }
+
+
+export {
+    renderWithHooks
+}
+
+
